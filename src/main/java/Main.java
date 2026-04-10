64171412cdf9885f033185cc3120252568f5cbb6
gdcraft07 <iklass987654321@gmail.com>
2026-04-10 11:39:57 +0300

    just testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+// Really cool bro
+// Fuck Wolfram Math
+
+
 class Main{
     static void main() throws IOException {
         try {
